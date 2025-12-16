5dbf3e8853db9055ed992d35240236dac0eec08a
Rami Daghlawi <rami.daghlawi@gmail.com>
2025-12-16 11:51:16 +0100

    add toggle

--- lua/slack/init.lua
+++ lua/slack/init.lua
@@ -1,1 +1,23 @@
-print("opened")
+local M = {
+	is_open = false,
+}
+function M.open()
+	print("opening")
+end
+
+function M.close()
+	print("closing")
+end
+
+function M.toggle()
+	if M.is_open then
+		M.close()
+		M.is_open = false
+	else
+		vim.cmd("tabnew")
+		M.open()
+		M.is_open = true
+	end
+end
+
+return M
